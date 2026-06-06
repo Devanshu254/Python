536f89b4dc5f03bd938d337fa0a9117159182006
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-06 12:13:25 +0530

    print function completed

--- Session_01/fist.py
+++ Session_01/fist.py
@@ -1,3 +1,7 @@
 print("Hello World")
 print("Hello World", 1, 1.5, True)
+# Seperator keyword.
 print("Hello World", 1, 1.2, True, sep="/")
+# Using end keyword.
+print("Hello", end="-")
+print("Devanshu")
