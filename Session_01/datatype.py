c66669f75fb52f1dbf9ba1044dc50e48e8663e0b
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-06 12:30:26 +0530

    data types

---
+++ Session_01/datatype.py
@@ -0,0 +1,31 @@
+# Integer
+print(8)
+print(1e308) # 1*10^308
+print(1e309)
+
+# Decimal/Float
+print(8.55)
+print(1.7e308)
+print(1.7e309)
+
+# Boolean
+print(True)
+print(False)
+
+# Text/String
+print("Hello World")
+
+# Complex Number
+print(5+6j)
+
+# List -> C -> Array
+print([1,2,3,4,5])
+
+# Tuple
+print((1,2,3,4,5))
+
+# Sets
+print({1,2,3,4,5})
+
+# Dictionary
+print({"Name": "Devanshu", "Gender": "Male"})
--- Session_01/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-for i in "Devanshu":
-#     print(i)
