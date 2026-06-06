674fd196469c6980a602b94b13cff7864d2444e8
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-06 15:33:29 +0530

    static, dynamic binding, dynamic typing

---
+++ Session_01/static_vs_dynamic_binding.py
@@ -0,0 +1,12 @@
+# Dynamic Binding is:
+a = 5
+print(5)
+a = 'nitish'
+print(a)
+
+# Output:
+# 5
+# nitish
+
+# Static binding is:
+# int a = 5, now this a will not be able to hold other value than integer.
--- Session_01/variables.py
+++ Session_01/variables.py
@@ -8,3 +8,10 @@
 # Python - We don't need to explicitly mention datatypes.
 name = "Devanshu"
 print(name)
+
+# Different ways to create variable and give them values in python.
+a,b,c = 1,2,3
+print(a,b,c)
+
+a=b=c= 5 # If we wanna give a, b and c the same value.
+print(a,c)
