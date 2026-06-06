368bde019570124f95c738dcca082fa6e19cba93
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-06 15:19:01 +0530

    variables_01

---
+++ Session_01/variables.py
@@ -0,0 +1,10 @@
+# Static vs Dynamic Typing.
+# Static vs Dynamic Binding.
+# Stylish declaration techniques.
+
+# C/C++
+# String name = "Devanshu"
+
+# Python - We don't need to explicitly mention datatypes.
+name = "Devanshu"
+print(name)
