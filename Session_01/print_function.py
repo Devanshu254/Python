533a3c17b6bfc23f3677e59fffa563cb39191cf8
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-06 12:35:42 +0530

    minor change

--- Session_01/print_function.py
+++ Session_01/print_function.py
@@ -1,3 +1,17 @@
+# Python is an interpreted language.
+
+# C/C++
+# Code → Compiler → Machine Code (.exe) → Run
+# Python ->
+# Python Code (.py)
+#        ↓
+# Python Interpreter
+#        ↓
+# Bytecode (.pyc)
+#        ↓
+# Python Virtual Machine (PVM)
+#        ↓
+# Run
 print("Hello World")
 print("Hello World", 1, 1.5, True)
 # Seperator keyword.
