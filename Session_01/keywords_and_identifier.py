714010aacdbd9aafe156f67b73aba48b8d929f13
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-06 20:36:14 +0530

    keywords and identifiers

---
+++ Session_01/keywords_and_identifier.py
@@ -0,0 +1,32 @@
+# A keyword in Python is a reserved word that has a predefined meaning and special purpose in the language, and it cannot be used as an identifier (such as a variable name or function name).
+
+# if = 10
+
+# if
+# else
+# for
+# while
+# def
+# class
+# return
+# True
+# False
+# None
+
+# Identifier.
+# An identifier in Python is a name used to identify a variable, function, class, module, or other object in a program.
+name = "Devanshu"
+age = 22
+# name → identifier
+# age → identifier
+
+def greet():
+    print("Hello")
+
+# greet → identifier
+# name      # ✅
+# _age      # ✅
+# marks123  # ✅
+
+# 123name   # ❌
+# if        # ❌ (keyword)
