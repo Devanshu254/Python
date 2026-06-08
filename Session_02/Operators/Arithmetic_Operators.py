1ab7f48fad5abe8ffd31ad12f61366511912c6e4
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-08 14:41:06 +0530

    arithmetic operator

---
+++ Session_02/Operators/Arithmetic_Operators.py
@@ -0,0 +1,13 @@
+print(5+6)
+
+print(5-6)
+
+print(5*6)
+
+print(5/2)
+
+print(5//2) # Integer Division - Convert normal division into integer.
+
+print(5%2) # It gives reminder.
+
+print(5**2) # Power of - 5^^2.
