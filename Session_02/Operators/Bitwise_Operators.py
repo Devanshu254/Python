bfd53c5c6918c0a29d6159734159df08227ca2e1
Devanshu <devanshujha8305@gmail.com>
2026-06-10 22:14:46 +0530

    bitwise_03

--- Session_02/Operators/Bitwise_Operators.py
+++ Session_02/Operators/Bitwise_Operators.py
@@ -1,2 +1,11 @@
+# Bitwise and - If both values are 1 then the output will be 1 otherwise 0. But first they will convert into binary and then the operation will be performed.
 print(2&3)
-print(2&4)
+
+# Bitwise or - If both values are 0 then the output will be 0 otherwise 1. But first they will convert into binary and then the operation will be performed.
+print(2|3)
+
+# Bitwise xor - If both values are same then the output will be 0 otherwise 1. But first they will convert into binary and then the operation will be performed.
+print(2^3)
+
+# Bitwise not - It will convert 1 to 0 and 0 to 1. But first they will convert into binary and then the operation will be performed.
+print(~2)
