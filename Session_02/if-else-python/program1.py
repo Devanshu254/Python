80093d0a213bbdd52070e86f8ea60ef44b7b62aa
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-12 22:34:48 +0530

    enhancement in code

--- Session_02/if-else-python/program1.py
+++ Session_02/if-else-python/program1.py
@@ -7,6 +7,13 @@ password = input("Enter the password: ")
 
 if email == 'devanshujha8305@gmail.com' and password == '1234':
     print("You are authorized")
+elif email == 'devanshujha8305@gmail.com' and password != '1234':
+    print("Password is wrong, please try again.")
+    password = input("Enter the password again: ")
+    if(password == '1234'):
+        print("You are authorized")
+    else:
+        print("You have attempted wrong password 2 times.. Please try again later.")
 else:
     print("You are not authorized")
 
