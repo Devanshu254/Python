23596979b4195395cd6bc5e2372591e67d8babb9
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-12 22:41:55 +0530

    min of three number program

---
+++ Session_02/if-else-python/min_number_print_program.py
@@ -0,0 +1,12 @@
+# Print the min of three numbers.
+
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+c = int(input("Enter number 3 "))
+
+if a<b and a<c:
+    print("a is min", a)
+elif b<a and b<c:
+    print("b is min", b)
+else:
+    print("c is min", c)
