1bb85328d77f3fd60f2df61bbef4f932adf65b43
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-12 21:49:13 +0530

    membership_operator

---
+++ Session_02/Operators/Membership_Operator.py
@@ -0,0 +1,18 @@
+# in/not in
+# Strings are case sensitive in python
+print('d' in 'Devanshu')
+
+print('d' not in 'Devanshu')
+
+print('De' in "Devanshu is good person")
+
+# print(1 in [1, 2, 3])
+
+# print(4 not in [1, 2, 3])
+
+# print('a' in {'a': 1, 'b': 2})
+
+# print(1 in {'a': 1, 'b': 2})
+
+# print(3 not in {'a': 1, 'b': 2})
+
