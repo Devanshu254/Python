3b967ed766c063c01602f77f3010583898ea9ba2
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-12 22:14:08 +0530

    if-else-loop first program

---
+++ Session_02/if-else-python/program1.py
@@ -0,0 +1,11 @@
+# Login program and indentation.
+# email -> devanshujha8305@gmail.com
+# password -> 1234
+
+email = input("Enter the email: ")
+password = input("Enter the password: ")
+
+if email == 'devanshujha8305@gmail.com' and password == '1234':
+    print("You are authorized")
+else:
+    print("You are not authorized")
