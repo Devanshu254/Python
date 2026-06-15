1695aa1cf922e19bdf1a3ad43a853ecdd3c1ec43
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-15 16:58:19 +0530

    module01

---
+++ Session_02/if-else-python/module.py
@@ -0,0 +1,7 @@
+# Module is a python file in which some functions are writtern, we can import those modules in our program so that we can use someone' else's function in our code.
+# Principle: Code Reusability.
+
+# Math
+# Keyword
+# Constant
+# Random
