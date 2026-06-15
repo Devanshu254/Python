34baedd152b9b57d161538833d8b8537bb574790
Devanshu Jha <devanshujha8305@gmail.com>
2026-06-15 17:07:32 +0530

    modules in python

--- Session_02/if-else-python/module.py
+++ Session_02/if-else-python/module.py
@@ -1,7 +1,28 @@
 # Module is a python file in which some functions are writtern, we can import those modules in our program so that we can use someone' else's function in our code.
 # Principle: Code Reusability.
 
-# Math
-# Keyword
-# Constant
-# Random
+# math
+# keyword
+# random
+# datetime
+
+# math
+import math
+print(math.factorial(5))
+print(math.sqrt(85))
+
+# keyword: reserved words in python. 
+# If we want to see how many reserved words are there in python then we can use this module.
+import keyword
+print(keyword.kwlist)
+
+# random: generate random values.
+import random
+print(random.randint(1,100))
+
+# datetime
+import datetime
+print(datetime.datetime.now())
+
+# How many modules are available in python.
+help('modules')
