c84fe4ecf9fcfe514948f4daa24401b1fcf05f6c
Devanshu Jha <devanshujha8305@gmail.com>
2026-07-29 16:00:14 +0530

    01_range

---
+++ Session_02/Loops_004/for_loops_004.py
@@ -0,0 +1,6 @@
+# Python for loop uses a function which is named as range.
+# If you give range function two numbers, then it would generate numbers within that range.
+
+# Printing 1 to 10
+for i in range(1,11): # in range, first number is included and last number is excluded.
+    print(i)
