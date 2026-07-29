e963d040016c1e121a5511db4a2600a0bdfacf3e
Devanshu Jha <devanshujha8305@gmail.com>
2026-07-29 16:02:02 +0530

    step_size_added

--- Session_02/Loops_004/for_loops_004.py
+++ Session_02/Loops_004/for_loops_004.py
@@ -2,5 +2,8 @@
 # If you give range function two numbers, then it would generate numbers within that range.
 
 # Printing 1 to 10
-for i in range(1,11): # in range, first number is included and last number is excluded.
+# for i in range(1,11): # in range, first number is included and last number is excluded.
+#     print(i)
+
+for i in range(1, 11, 2): # here 2 is stepsize.
     print(i)
