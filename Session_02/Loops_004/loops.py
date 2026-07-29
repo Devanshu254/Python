fb38755a02c555cc6bde0e6450addf1223a654f3
Devanshu Jha <devanshujha8305@gmail.com>
2026-07-29 12:10:54 +0530

    loops_001

---
+++ Session_02/Loops_004/loops.py
@@ -0,0 +1,4 @@
+# With the help of loops, we can use a peice of code multiple times.
+# While Loop Example: Program to print table
+# Program: Sum of all digits of given number
+# Program: Keep accepting numbers from user until he/she enters a 0 and then find average.
