e18594d2caad37d28b0e882fcb841b7e4b9908b4
Devanshu Jha <devanshujha8305@gmail.com>
2026-07-29 16:14:11 +0530

    comit

--- Session_02/Loops_004/for_loops_004.py
+++ Session_02/Loops_004/for_loops_004.py
@@ -5,5 +5,16 @@
 # for i in range(1,11): # in range, first number is included and last number is excluded.
 #     print(i)
 
-for i in range(1, 11, 2): # here 2 is stepsize.
-    print(i)
+# for i in range(1, 11, 2): # here 2 is stepsize.
+#     print(i)
+
+# for i in range(10, 0,-1): # Going backward.
+#     print(i)
+
+# for i in 'Delhi':
+#     print(i)
+
+# for i in [1,2,3,4,5]: 
+    # print(i)
+
+# We can iterate over dictionaries, sets and tuples also.
