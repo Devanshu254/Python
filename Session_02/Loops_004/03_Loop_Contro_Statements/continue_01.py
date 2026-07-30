ee560b5722d558602ffdcbddaf5c8387f60dbc68
Devanshu Jha <devanshujha8305@gmail.com>
2026-07-30 22:54:20 +0530

    continue_example

--- Session_02/Loops_004/03_Loop_Contro_Statements/continue_01.py
+++ Session_02/Loops_004/03_Loop_Contro_Statements/continue_01.py
@@ -1,5 +1,11 @@
+"""
+Real life example where a programmer will use continue
+When we want to skip a product in e-commerce. We don't want to show those items whose stock is 0.
+"""
 # In continue we skip the iteration.
 for i in range(1,10):
     if i==5:
         continue
     print(i)
+
+
