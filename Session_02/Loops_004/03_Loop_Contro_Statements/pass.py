cb0f396c017da64e3525fb1e5f2d7cab967e65b1
Devanshu Jha <devanshujha8305@gmail.com>
2026-07-30 22:57:42 +0530

    pass

---
+++ Session_02/Loops_004/03_Loop_Contro_Statements/pass.py
@@ -0,0 +1,29 @@
+"""
+Suppose you're building an e-commerce application.
+
+Today you're designing the classes, but you'll implement them later.
+
+class Animal:
+
+    def speak(self):
+        pass
+class Dog(Animal):
+
+    def speak(self):
+        print("Bark")
+
+class User:
+    pass
+
+class Product:
+    pass
+
+class Order:
+    pass
+
+class Payment:
+    pass
+"""
+
+for i in range(1,10):
+    pass
