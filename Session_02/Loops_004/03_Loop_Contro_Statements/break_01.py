0694e6c22a7ae33d84156b4f34afcc8d45449e86
Devanshu Jha <devanshujha8305@gmail.com>
2026-07-30 22:38:09 +0530

    break_example

--- Session_02/Loops_004/03_Loop_Contro_Statements/break_01.py
+++ Session_02/Loops_004/03_Loop_Contro_Statements/break_01.py
@@ -8,3 +8,6 @@ for i in range(1,10):
     if i==5:
         break
     print(i)
+
+# Example scenario where we can use break.
+# If we are searching someone's name in database. Database is so huge so if we just use break statement we won't go after that particular person or number.
