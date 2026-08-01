c80ba8b05aff423ec8b83d8be5c39576ef4d282c
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-01 19:14:23 +0530

    slicing

--- Session_03/Strings_01/Indexing_03.py
+++ Session_03/Strings_01/Indexing_03.py
@@ -1,9 +1,21 @@
 # How to access substrings from string.
 
-# Positive Indexing.
-s = 'hello world'
-print(s[6])
+# # Positive Indexing.
+# s = 'hello world'
+# print(s[6])
 
-# Negative Indexing.
+# # Negative Indexing.
+# s = 'hello world'
+# print(s[-1])
+
+# Slicing.
 s = 'hello world'
-print(s[-1])
+print(s[0:4])
+print(s[1:]) # Going from 1st to last.
+print(s[:5]) # Going from fist to 5th.
+print(s[:]) # Print complete string.
+# Stepsize.
+print(s[0:6:2])
+# We can use negative indexing as well.
+print(s[-1:-6:-1])
+print(s[6:0:-1])
