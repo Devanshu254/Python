bfcdd989a3ebcb2f5228148a457d50a29374cf30
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-01 19:05:36 +0530

    indexing

---
+++ Session_03/Strings_01/Indexing_03.py
@@ -0,0 +1,9 @@
+# How to access substrings from string.
+
+# Positive Indexing.
+s = 'hello world'
+print(s[6])
+
+# Negative Indexing.
+s = 'hello world'
+print(s[-1])
--- Session_03/Strings_01/creating_string_02.py
+++ Session_03/Strings_01/creating_string_02.py
@@ -6,3 +6,6 @@ s = '''hello'''
 s = """hello"""
 print(s)
 
+s = str('hello')
+print(s)
+
