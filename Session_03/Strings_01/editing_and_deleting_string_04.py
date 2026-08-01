bb1679248b7b836a536b38a6dbf8de5013f0a191
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-01 19:28:35 +0530

    unreferencing_string

---
+++ Session_03/Strings_01/editing_and_deleting_string_04.py
@@ -0,0 +1,8 @@
+s = 'hello world'
+# s[0] = 'H' -> This will throw an error, strings are immutable in python.
+# Strings can not be edited.
+
+# Deletion in string. 
+# There is one del function.
+del s # It's getting unreferenced.
+print(s)
