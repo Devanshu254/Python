8c3d83f1984bccf0a6fbc215ccaf243aeeab613b
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-03 00:16:43 +0530

    functions_07

---
+++ Session_03/String_Functions_03/functions_07.py
@@ -0,0 +1,20 @@
+"""
+1. split
+2. join
+3. replace
+4. strip -> removes the spaces.
+"""
+print('hi my name is Devanshu'.split())
+# ['hi', 'my', 'name', 'is', 'Devanshu']
+
+print('hi my name is Devanshu'.split('is'))
+# ['hi my name ', ' Devanshu']
+
+print("-".join(['hi','my','name','is','Devanshu']))
+# hi-my-name-is-Devanshu
+
+print('hi my name is nitish'.replace('nitish', 'Devanshu'))
+# hi my name is Devanshu
+
+print('Devanshu   '.strip())
+# Devanshu
