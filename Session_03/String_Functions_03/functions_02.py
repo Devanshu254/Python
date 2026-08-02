8b633fa3254516e31e9604256c0859b1e45299d0
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-02 23:31:30 +0530

    functions_02

---
+++ Session_03/String_Functions_03/functions_02.py
@@ -0,0 +1,19 @@
+"""
+Functions that will specifically work for string.
+Capitalize/Title/Upper/Lower/Swapcase.
+"""
+s = 'hello world'
+print(s.capitalize())
+# Hello world
+
+print(s.title())
+# Hello World
+
+print(s.upper())
+# HELLO WORLD
+
+print(s.lower())
+# hello world
+
+print(s.swapcase()) # -> This function will turn lower one to upper and upper character to lower.
+# HELLO WORLD 
