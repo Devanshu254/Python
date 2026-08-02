48abf4f77bb615cb54a233c3b43528deb2054ab2
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-02 23:16:35 +0530

    logical_operator

---
+++ Session_03/Operations_On_String_02/logical_operator_04.py
@@ -0,0 +1,13 @@
+# If a string contains characters then it's true and if the string is empty then it's false.
+print('hello' and 'world') # True and True == True. In 'and' the output will be the second string.
+# Output:world
+print('hello' or 'world') # In case of 'or' the output will be the first string always.
+# Output: hello
+print('' and world)
+# Output: empty
+print('' or 'world')
+# Output: world
+print(not '')
+# Output: True
+print(not 'hello')
+# Output: False
