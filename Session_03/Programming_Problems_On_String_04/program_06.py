daf8abd703d2e52b0d5726c1620509c139da7425
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-04 20:48:48 +0530

    split

---
+++ Session_03/Programming_Problems_On_String_04/program_06.py
@@ -0,0 +1,13 @@
+"""
+Write a program to count number of words in a string with and without using split() function.
+"""
+# Using Split.
+s = input("Enter the string: ")
+
+words = s.split()
+count = len(words)
+
+print("Number of words ", count)
+
+# Without using split - see program_07
+
