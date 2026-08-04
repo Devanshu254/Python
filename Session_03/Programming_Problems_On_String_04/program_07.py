5db919b89947070c9affc8a80264eba43f18c887
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-04 20:59:34 +0530

    program_07

---
+++ Session_03/Programming_Problems_On_String_04/program_07.py
@@ -0,0 +1,8 @@
+# Write a program that can convert integer into string.
+
+num = int(input("Enter the number: "))
+
+s = str(num)
+print("String ",s)
+print("Type of num: ", type(num))
+print("Type of s: ", type(s))
