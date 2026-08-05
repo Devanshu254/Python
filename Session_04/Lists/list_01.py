63c258f7e86619770fb300ae487eb9af74ff798b
Devanshu Jha <devanshujha8305@gmail.com>
2026-08-05 19:38:41 +0530

    list_01

---
+++ Session_04/Lists/list_01.py
@@ -0,0 +1,11 @@
+"""
+What are Lists?
+Lists Vs Arrays
+Characterstics of a List
+How to create a list
+Access items from a List
+Editing items in a List
+Deleting items from a List
+Operations on Lists
+Functions on Lists
+"""
